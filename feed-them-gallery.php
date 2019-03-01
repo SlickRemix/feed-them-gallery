73bf74a6b0e182ace459691f27fb732f3b1abf50
Spencer <spencer@slickremix.com>
2019-03-01 17:30:09 -0500

    1.1.5.1

--- feed-them-gallery.php
+++ feed-them-gallery.php
@@ -5,29 +5,29 @@
  * This class is what initiates the Feed Them Gallery class
  *
  * Plugin Name: Feed Them Gallery
- * Plugin URI: http://slickremix.com/
+ * Plugin URI: https://slickremix.com/
  * Description: Create Beautiful Responsive Galleries in Minutes. Choose the number of columns a loadmore button, popup and more!  Sell your galleries or individual images, watermark them and even zip galleries with our premium version.
- * Version: 1.1.4
+ * Version: 1.1.5.1
  * Author: SlickRemix
- * Author URI: http://slickremix.com/
+ * Author URI: https://www.slickremix.com/
  * Text Domain: feed-them-gallery
  * Domain Path: /languages
  * Requires at least: Wordpress 4.7.0
- * Tested up to: WordPress 4.9.8
- * Stable tag: 1.1.4
+ * Tested up to: WordPress 5.1
+ * Stable tag: 1.1.5.1
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * WC requires at least: 3.0.0
  * WC tested up to: 3.5.1
  *
- * @version  1.1.4
+ * @version  1.1.5.1
  * @package  FeedThemSocial/Core
- * @copyright  	Copyright (c) 2012-2018 SlickRemix
+ * @copyright  	Copyright (c) 2012-2019 SlickRemix
  *
  * Need Support? http://www.slickremix.com/my-account
  */
 // Makes sure any js or css changes are reloaded properly. Added to enqued css and js files throughout
-define('FTG_CURRENT_VERSION', '1.1.4');
+define('FTG_CURRENT_VERSION', '1.1.5.1');
 
 final class Feed_Them_Gallery {
 
@@ -67,7 +67,6 @@ final class Feed_Them_Gallery {
                 }
             } // end ftg_required_php_check
 
-
             register_activation_hook(__FILE__, array(self::$instance, 'ftg_activate'));
             add_action('admin_notices', array(self::$instance, 'ft_gallery_display_install_notice'));
             add_action('admin_notices', array(self::$instance, 'ft_gallery_display_update_notice'));
@@ -96,7 +95,7 @@ final class Feed_Them_Gallery {
             self::$instance->settings_page = new feed_them_gallery\Settings_Page();
 
             //Media Taxonomies
-           // self::$instance->media_taxonomies = new feed_them_gallery\Media_Taxonomies();
+            // self::$instance->media_taxonomies = new feed_them_gallery\Media_Taxonomies();
 
             //Setup Plugin functions
             self::$instance->setup_functions = new feed_them_gallery\Setup_Functions();
@@ -291,7 +290,7 @@ final class Feed_Them_Gallery {
         include(FEED_THEM_GALLERY_PLUGIN_FOLDER_DIR . 'admin/settings-page.php');
 
         //Tags/Taxonomies for images
-       // include(FEED_THEM_GALLERY_PLUGIN_FOLDER_DIR . 'includes/taxonomies/media-taxonomies.php');
+        // include(FEED_THEM_GALLERY_PLUGIN_FOLDER_DIR . 'includes/taxonomies/media-taxonomies.php');
 
         //Setup Functions Class
         include(FEED_THEM_GALLERY_PLUGIN_FOLDER_DIR . 'includes/setup-functions-class.php');
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: slickremix
 Tags: gallery, image gallery, photo gallery, responsive gallery, wordpress gallery plugin
 Requires at least: 4.5.0
-Tested up to: 5.0.2
-Stable tag: 1.1.5
+Tested up to: 5.1
+Stable tag: 1.1.5.1
 License: GPLv2 or later
 
 Create customizable Image Galleries and sell your images using WooCommerce.
@@ -83,7 +83,6 @@ FREE PLUGIN OPTIONS
  * Say you set 10 image to show and you have 50 images. Then you would see 10 of 50. And when you loadmore the number will increase.
  * Change the color of the pagination numbers
 
-
 [PREMIUM PLUGIN FEATURES](https://www.slickremix.com/downloads/feed-them-gallery/)
 = Watermark Options =
  * Disable the right click option for desktop computers
@@ -127,11 +126,15 @@ See [Full Documentation](https://www.slickremix.com/feed-them-gallery/)
   * Extract the zip file and drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
 
 == Changelog ==
+= Version 1.1.5.1 Friday, March 1st, 2019 =
+  * Works with Wordpress 5.1
+  * Text Updates
+
 = Version 1.1.5 Wednesday, November 28th, 2018 =
   * FIX: Watermark button to add an image
   * FIX: Customizing the Purchase Product button
   * FIX: Custom username and link option
-  * FIX: Extra spacing option showing for all image gallery types
+  * FIX: Extra Image spacing option showing for layout types
 
 = Version 1.1.4 Wednesday, November 21st, 2018 =
   * FIX: Error causing customizer to not load correctly.
