b941142e3ade14ea8c1fd6ba35937f87c08bfe10
Justin Labadie <50183104+justinslickremix@users.noreply.github.com>
2019-10-16 16:09:50 -0400

    Update feed-them-gallery.php
    1.2.5.1

--- feed-them-gallery.php
+++ feed-them-gallery.php
@@ -7,20 +7,20 @@
  * Plugin Name: Feed Them Gallery
  * Plugin URI: https://www.slickremix.com/
  * Description: Create Beautiful Responsive Galleries in Minutes. Choose the number of columns, loadmore button, popup and more! Sell your Galleries or individual Images with WooCommerce, watermark them, zip galleries, create Albums, create Tags for Images and Galleries, search Galleries and Images with tags, and pagination in our premium version.
- * Version: 1.2.5
+ * Version: 1.2.5.1
  * Author: SlickRemix
  * Author URI: https://www.slickremix.com/
  * Text Domain: feed-them-gallery
  * Domain Path: /languages
  * Requires at least: WordPress 4.7.0
- * Tested up to: WordPress 5.2.3
- * Stable tag: 1.2.5
+ * Tested up to: WordPress 5.2.4
+ * Stable tag: 1.2.5.1
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * WC requires at least: 3.0.0
- * WC tested up to: 3.7.0
+ * WC tested up to: 3.7.1
  *
- * @version  1.2.5
+ * @version  1.2.5.1
  * @package  FeedThemSocial/Core
  * @copyright   Copyright (c) 2012-2019 SlickRemix
  *
@@ -28,7 +28,7 @@
  */
 
 // Doing this ensure's any js or css changes are reloaded properly. Added to enqued css and js files throughout.
-define( 'FTG_CURRENT_VERSION', '1.2.5' );
+define( 'FTG_CURRENT_VERSION', '1.2.5.1' );
 
 // Require file for plugin loading.
 require_once __DIR__ . '/class-load-plugin.php';
